b09dcc88ff717cc0bd4c4ce3114e3ace797b08bd
jack.jin <hellowjs111>
2023-05-06 16:46:36 +0800

    code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router';
+import { KeepAliveProvider } from 'react-next-keep-alive';
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  return (
+    <KeepAliveProvider router={router}>
+      <Component {...pageProps} />
+    </KeepAliveProvider>
+  )
 }
